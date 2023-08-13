f0c634ea4370c4dd99f5c1b827fc35d426031bab
Shewanji <marrionwanjiru26@gmail.com>
2023-08-13 09:51:18 -0700

    add console prompt tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -43,5 +43,34 @@ class TestConsole(unittest.TestCase):
         self.create = None
 
 
+class ConsolePromptTest(unittest.TestCase):
+    """Console class test cases."""
+    def test_Console_prompt(self):
+        """Check the prompt"""
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_Console_prompt_empty_line(self):
+        """Check empty line."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue())
+
+    def test_Console_prompt_new_line(self):
+        """Check new line."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("\n"))
+            self.assertEqual("", output.getvalue())
+
+    def test_Console_quit(self):
+        """Check quit exists."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_Console_eof(self):
+        """Check eof exists."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
+
+
 if __name__ == "__main__":
     unittest.main()
